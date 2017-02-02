668387ee6ed11f7704934ad58f5be5c5936a3e97
Khushboo Bhatia <khush@cmu.edu>
2017-02-02 13:47:04 -0500

    Separate db and routing layer code

--- src/middleware/location_manager.js
+++ src/middleware/location_manager.js
@@ -1,15 +1,98 @@
 
 var Location = require('../models/location')
+var Gateway = require('../models/gateway');
+var Device = require('../models/device');
+
+exports.getRootLocation = function(id, callback){
+    //TODO: Change it to return root only
+    Location.find().exec( callback);
+}
+exports.getLocationById = function(id, callback){
+	Location.findById(id, function (err, result) {
+        if(err) callback(err);
+        if (result == null ) { 
+            var error = new Error();
+            error.message = 'Could not find a location with id :'+ id ;
+            callback(error);
+        }
+        callback(null, result);
+    })
+}
+
+exports.createNewLocation = function(req, callback){
+    //TODO: Add validation for not null name and type and geoloc
+    var location = new Location(req.body);
+    location.save(callback);
+};
+
+exports.createNewChildLocation = function(req, callback){
+    var newLocation = new Location(req.body);
+    var parentId = req.params._id;
+
+    newLocation.save( function(err, result){
+        if(err) { return callback(err); }
+        newLocation = result;
+    })
+
+    Location.findByIdAndUpdate(parentId, { $addToSet: { children: newLocation._id }}, function (err, result) {
+        if(err) {
+            //Rollback.. Delete the newly created location
+            console.log("Error in adding to parent");
+            newLocation.remove( function(err) { 
+                if(err) return callback(err); 
+            })
+            return callback(err);
+        }   
+        console.log("Added reference to parent "+result);
+        callback(null, result);
+    })
+};
 
 exports.updateLocation = function(req, callback){
-    console.log("Inside location manager");
 	var locationToUpdate = req.location;
+    //TODO : Add other fields
  	if(typeof req.body.name != 'undefined') locationToUpdate.name = req.body.name;
  	if(typeof req.body.test != 'undefined') locationToUpdate.test = req.body.test;
-    console.log("About to save");
+   
  	locationToUpdate.save( function(err, result){
         callback(err, result);
  	})  
 };
 
+exports.deleteLocation = function(req, callback){
+    locationToDelete = req.location;
+    var children = locationToDelete.children;
+    if (children != null && children.length > 0){
+        var error = new Error();
+        error.message =  "Location is not empty. Cannot delete it.";
+        callback(error);
+    }  
+    // TODO: Can't delete a location if there are devices and gateways pointing to it.
+    
+    // Search for parent and remove child reference
+    Location.findOneAndUpdate({ children: req.params._id}, { $pull: { children: req.params._id}}, function (err, result) {
+        if(err) { return callback(err); }
+    })
+
+    locationToDelete.remove(function(err){
+        if(err) { return callback(err); }
+    })
+    var result = new Object();
+    result.message = "Delete successful";
+    callback(null, result);
+};
+
+exports.getGateways = function(req, callback){
+    //TODO: Change it to return all gateways in child locations too only 
+    // if it is a building or with a deep flag
+    Gateway.find({ location_id : req.params._id }).exec(callback);
+    
+};
+
+exports.getDevices = function(req, callback){
+   //TODO: Change it to return all gateways in child locations too only 
+    // if it is a building or with a deep flag
+    Device.find({ location_id : req.params._id }).exec(callback);
+    
+};
 module.exports = exports;
--- src/routes/location.js
+++ src/routes/location.js
@@ -2,42 +2,36 @@ var express = require('express');
 var router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
 //Models
-var Location = require('../models/location')
-var Gateway = require('../models/gateway');
-var Device = require('../models/device');
+
 var locationManager = require('../middleware/location_manager');
 
 /* GET all locations. */
 router.get('/', function(req, res, next) {
- 	Location.find().exec(function (err, result) {
- 		if(err) { return next(err); }
-  		res.json(result);
-	})
+ 	locationManager.getRootLocation( function(err, result){
+        if(err) { return next(err); }
+        res.json(result);
+    })
 });
 
 /* Create new location */
 router.post('/', function(req, res, next) {
-	var location = new Location(req.body);
-	location.save(function (err, result) {
-		if(err) { return next(err); }
-  		res.json(result);
-	})
+	locationManager.createNewLocation(req, function(err, result){
+        if(err) { return next(err); }
+        res.json(result);
+
+    })
+	
 });
 
 /*Validate _id in all request URLs*/
 router.param('_id', function(req, res, next, id) {
     if(!ObjectId.isValid(id)){
         var error = new Error();
-        error.message = "Invalid Object ID "+id ;
+        error.message = "Invalid Object ID " + id ;
         return next(error);
     }
-    Location.findById(id, function (err, result) {
-        if (err) next(err);
-        if (result == null ) { 
-            var error = new Error();
-            error.message = 'Could not find a location with id :'+ id ;
-            return next(error);
-        }
+    locationManager.getLocationById(id, function(err, result) {
+        if(err) { return next(err); }
         req.location = result;
         next();
     })
@@ -48,86 +42,46 @@ router.get('/:_id', function(req, res, next) {
  	return res.json(req.location);
 });
 
-/* Get all gateways in this location */
+/* Get all gateways at this location */
 router.get('/:_id/gateways', function(req, res, next) {
-    var location = req.location.toObject();
-    Gateway.find({ location_id : req.params._id }).exec(function (err, result) {
+    locationManager.getGateways( req, function(err, result){
         if(err) { return next(err); }
-        location.gateways = result;
-        return res.json(location);
+        return res.json(result);
     })
     
 });
 
-/* Get all devices in this location */
+/* Get all devices at this location */
 router.get('/:_id/devices', function(req, res, next) {
-    var location = req.location.toObject();
-    Device.find({ location_id : req.params._id }).exec(function (err, result) {
+    locationManager.getDevices( req, function(err, result){
         if(err) { return next(err); }
-        location.devices = result;
-        return res.json(location);
+        return res.json(result);
     })
-  
 });
 
 /* Create new child location */
 router.post('/:_id', function(req, res, next) {
-	var newLocation = new Location(req.body);
-	newLocation.save( function (err, result) {
-	    if(err) { return next(err); }
-        newLocation = result;
-    })
-
-    console.log("Created new location " + newLocation);
-	Location.findByIdAndUpdate(req.params._id, { $addToSet: { children: newLocation._id }}, function (err, result) {
-        if(err) { 
- 			console.log("Error in adding to parent");
-            Location.findByIdAndRemove(newLocation._id , function (err, result){})
- 			return next(err);
- 		}	
-        console.log("Added reference to parent "+result);
-		res.json(newLocation);
-	})
-			
+	locationManager.createNewChildLocation(req, function(err, result){
+        if(err) { return next(err); }
+        return res.json(result);
+    })			
 });
 
 /* Update a location */
 router.put('/:_id', function(req, res, next) {
-   /* var locationToUpdate = req.location;
-    if(typeof req.body.name != 'undefined') locationToUpdate.name = req.body.name;
-    if(typeof req.body.test != 'undefined' ) locationToUpdate.test = req.body.test;
-    locationToUpdate.save( function(err, result){
-    if(err) { return next(err); }
-          return res.json(result);
-    })*/
+    locationManager.updateLocation(req, function(err, result) {
+        if(err) {  return next(err); }      
+        return res.json(result);
+    })
 
-        locationManager.updateLocation(req, function(err, result) {
-                if(err) { console.log("inside error "); return next(err); }
-                console.log("returning result in router");
-                return res.json(result);
-              })
 });
 
 /* Delete a location */
 router.delete('/:_id', function(req, res, next) {
-	locationToDelete = req.location;
-    var children = locationToDelete.children;
-    if (children != null && children.length > 0){
-        console.log("Can't delete location that has children");   
-        res.json({ error : { message: "Location is not empty. Cannot delete it."}});
-    }  
-    // TODO: Can't delete a location if there are devices and gateways pointing to it.
-    
-    // Search for parent and remove child reference
-    Location.findOneAndUpdate({ children: req.params._id}, { $pull: { children: req.params._id}}, function (err, result) {
+	locationManager.deleteLocation(req, function(err, result){
         if(err) { return next(err); }
- 	})
-
-    locationToDelete.remove(function(err){
-        if(err) { return next(err); }
-    })
-
-    res.json({message: 'Delete successful'});
+        return res.json(result);
+    } )
 	
 });
 
