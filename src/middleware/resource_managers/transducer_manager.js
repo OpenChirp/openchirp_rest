4817bcd8e9533c2739466e4a483e98c481dfe5e2
Khushboo Bhatia <khush@cmu.edu>
2017-04-29 00:48:02 -0400

    Time Series Database last value api for transducers

--- src/middleware/resource_managers/transducer_manager.js
+++ src/middleware/resource_managers/transducer_manager.js
@@ -1,5 +1,8 @@
 var Device = require('../../models/device');
 var mqttClient = require('../pubsub/mqtt_client');
+var request = require('request');
+var async = require('async');
+var nconf = require('nconf');
 
 exports.createDeviceTransducer = function(req, callback ){
 	req.device.transducers.push(req.body);
@@ -10,10 +13,46 @@ exports.getAllDeviceTransducers = function(req, callback ){
 	var deviceId = req.device._id;
 	Device.findById(deviceId).exec(function(err, result){
 		if(err) { return callback(err); } 
-		//TODO: Get last value from tsdb api
-		var transducers = result.transducers;
-		return callback(null, transducers) ;
+		getTransducerLastValue(result, callback);
+		
 	})
+};
+
+var getTransducerLastValue = function(device, callback){
+	var transducers  = device.transducers;
+	var measurements = [];
+	var lastValue = {};
+	var getFromInfluxdb = function(measurement, index, next){
+		var url = "http://"+ nconf.get('influxdb:host') + ":" + nconf.get("influxdb:port") +"/query" ;
+		console.log(" url: "+url);
+
+		var query = "select \"value\" from \""+measurement+"\" ORDER BY time DESC LIMIT 1";
+		console.log("query: "+query);
+		var props = {
+			"db" : "openchirp",
+			"q" : query
+		};
+        request({url : url, qs : props}, function(err, response, body) {
+  			if(err) { console.log(err);  }
+			 console.log("Get response: " + response.statusCode);
+			 console.log("Body "+body);
+			 if(body.results && body.results.length >0){
+			 	value = body.results[0].series[0].values;
+			 	lastValue[index] = value[0];
+			 }
+			 return next(null, null);
+		});
+	};
+	transducers.forEach(function(tdc){
+		measurements.push(device._id+"_"+tdc.name.toLowerCase());		
+	})
+
+	async.forEachOf(measurements, getFromInfluxdb, function(err, result) {
+
+		return callback(null, transducers);
+
+	})
+
 };
 
 exports.publishToDeviceTransducer = function(req, callback ){
