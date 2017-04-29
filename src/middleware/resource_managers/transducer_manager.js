7d8682e8d55482b990872118cb56e2dde9a239a2
Khushboo Bhatia <khush@cmu.edu>
2017-04-29 01:26:36 -0400

    Transducer timeseries api for last value

--- src/middleware/resource_managers/transducer_manager.js
+++ src/middleware/resource_managers/transducer_manager.js
@@ -21,7 +21,7 @@ exports.getAllDeviceTransducers = function(req, callback ){
 var getTransducerLastValue = function(device, callback){
 	var transducers  = device.transducers;
 	var measurements = [];
-	var lastValue = {};
+	var lastValues = {};
 	var getFromInfluxdb = function(measurement, index, next){
 		var url = "http://"+ nconf.get('influxdb:host') + ":" + nconf.get("influxdb:port") +"/query" ;
 		console.log(" url: "+url);
@@ -35,15 +35,18 @@ var getTransducerLastValue = function(device, callback){
         request({url : url, qs : props}, function(err, response, body) {
   			if(err) { console.log(err);  }
 			 console.log("Get response: " + response.statusCode);
-			 console.log("Body "+body);
+			 console.log("Body " + body);
 			 var data  = JSON.parse(body);
 			 if(data.results && data.results.length >0){
 			 	console.log("result length " + data.results.length);
 			 	var series = data.results[0].series;
 			 	if(series && series.length >0 ){
 			 		var values = series[0].values;
-			 		console.log("values " + values);			 	
-			 		lastValue[index] = values[0];
+			 		console.log("values " + values);	
+			 		lastValues[index] = {};		 	
+			 		lastValues[index].timestamp  = values[0];
+			 		lastValues[index].value = values[1];
+			 		console.log("lastValues : " +lastValues[index]);
 				 }
 			 }
 			 return next(null, null);
@@ -55,7 +58,8 @@ var getTransducerLastValue = function(device, callback){
 
 	async.forEachOf(measurements, getFromInfluxdb, function(err, result) {
 		for (var i = 0; i < transducers.length ; i++){
-			transducers[i].lastValue = lastValue[i];
+			console.log("lastValue in async loop " + lastValues[i]);
+			transducers[i].lastValue = lastValues[i];
 		}
 		return callback(null, transducers);
 
