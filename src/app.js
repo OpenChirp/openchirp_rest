64f9c2c32cc284c8d8585dc14ed356443cdfb655
Khushboo Bhatia <khush@cmu.edu>
2017-05-03 15:33:01 -0400

    CORS headers

--- src/app.js
+++ src/app.js
@@ -33,10 +33,11 @@ nconf.file({ file: filename });
 var allowCrossDomain = function(req, res, next) {
     var origin = req.get('origin');
     res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    //res.header('Access-Control-Allow-Headers', 'Accept, Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
+    //res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    //res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Accept, Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
 };
 
