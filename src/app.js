8011b628c4550ecc576c2c0a67f289a96cb6c768
khush-bhatia <khush@cmu.edu>
2017-05-03 20:48:33 -0400

    Fixing cors OPTIONS issue

--- src/app.js
+++ src/app.js
@@ -33,12 +33,17 @@ nconf.file({ file: filename });
 var allowCrossDomain = function(req, res, next) {
     var origin = req.get('origin');
     res.header('Access-Control-Allow-Origin', origin);
-    //res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    //res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Accept, Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
+
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+    
 };
 
 app.use(allowCrossDomain);
