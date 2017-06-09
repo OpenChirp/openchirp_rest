ee199abd22a7748dae96ba022bbcb52f6589cee5
Khushboo Bhatia <khush@cmu.edu>
2017-06-09 17:33:54 -0400

    Token manager

---
+++ src/middleware/resource_managers/thing_token_manager.js
@@ -0,0 +1,20 @@
+var ThingToken = require('../../models/thing_token');
+
+
+exports.createToken = function(req, callback){
+
+};
+
+exports.getToken = function(req,callback){
+
+};
+
+exports.recreateToken = function(req, callback){
+
+};
+
+exports.deleteToken = function(req, callback){
+
+};
+
+module.exports = exports;
