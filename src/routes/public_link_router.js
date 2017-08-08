d15db5e01d2375a013cda0805c5cb8da73a64196
khush-bhatia <khush@cmu.edu>
2017-08-08 13:11:59 -0400

    Public link get and post

--- src/routes/public_link_router.js
+++ src/routes/public_link_router.js
@@ -7,8 +7,8 @@ var publicLinkManager = require('../middleware/resource_managers/public_link_man
 router.get('/:_payload', function(req, res, next) {
 	console.log("original URL " + req.originalUrl);
 	console.log(" URL " + req.url);
-	console.log(" host " + req.host);
-	var link = nconf.get("pc_base_url") + "/pc/"+payload;
+	console.log(" host " + req.hostname);
+	var link = String(nconf.get('pc_base_url')) + "/pc/"+_payload;
 	console.log(link);
 	var html = '<html><title>Run Command On Openchirp</title><form method="POST" action="' +link +'"><button type="submit" class="btn btn-success">Run</button></form></html>';
     res.send(html);
