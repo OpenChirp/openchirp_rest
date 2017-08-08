7b9371c8460a91990010e384b21ddadb95a42591
khush-bhatia <khush@cmu.edu>
2017-08-08 12:32:03 -0400

    Public link get and post

--- src/routes/public_link_router.js
+++ src/routes/public_link_router.js
@@ -4,7 +4,10 @@ var router = express.Router();
 var publicLinkManager = require('../middleware/resource_managers/public_link_manager');
 
 router.get('/:_payload', function(req, res, next) {
-	var html = "<html><title>Run Command</title><body>Test</body></html>"
+	console.log("original URL" + req.originalUrl);
+	console.log(" URL" + req.url);
+		console.log(" host" + req.host);
+	var html = "<html><title>Run Command On Openchirp</title><body>Test</body></html>"
     res.send(html);
 });
 router.post('/:_payload', function(req, res, next) {
