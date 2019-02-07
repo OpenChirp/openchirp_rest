d6c12f4de2896b324ad6285f71168d13c5bcd7e9
Craig Hesling <craig@hesling.com>
2019-02-07 14:01:19 -0500

    Added note about what the schema options do

--- src/models/schema_options_notimestamp.js
+++ src/models/schema_options_notimestamp.js
@@ -1,4 +1,6 @@
 
+// Among other things, the following two options make the "id" field
+// appear in the queried object. The _id field remains present.
 exports = {
     toObject: { virtuals: true },
     toJSON: { virtuals: true }
